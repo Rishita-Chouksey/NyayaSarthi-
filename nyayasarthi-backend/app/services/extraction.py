86f32af5a090fbdcb4a4de673f9bb916a47ba404
Yash Sahu <ysahu9811@gmail.com>
2026-07-20 22:37:12 +0530

    Update extraction.py

--- nyayasarthi-backend/app/services/extraction.py
+++ nyayasarthi-backend/app/services/extraction.py
@@ -103,7 +103,7 @@ def run_ai_extraction(judgment_text: str) -> dict:
     print("Sending request to Gemini...")
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemini-2.5-pro",
         contents=prompt,
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
